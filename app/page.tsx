4c30d30a8ad3a2571779bfd0436a98776a3ba44b
naffisya <rindifathul@gmail.com>
2025-10-23 10:37:31 +0700

    fix: add react-icons dependency

--- app/page.tsx
+++ app/page.tsx
@@ -8,11 +8,13 @@ import {
   SiNextdotjs,
   SiTailwindcss,
 } from "react-icons/si";
+import Navbar from "./components/navbar";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black text-white flex flex-col items-center justify-center px-6 py-12">
-      
+      <Navbar />
+
       {/* FOTO */}
       <Image
         src="/rin.png"
